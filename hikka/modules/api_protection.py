2cdfb42886a9d4063f15fb03a34abadc5e88225a
evllf <het.ctpaxa.ot.cbata@gmail.com>
2025-02-05 06:32:47 +0600

    Update api_protection.py

--- hikka/modules/api_protection.py
+++ hikka/modules/api_protection.py
@@ -3,6 +3,7 @@
 # 🌐 https://github.com/hikariatama/Hikka
 # You can redistribute it and/or modify it under the terms of the GNU AGPLv3
 # 🔑 https://www.gnu.org/licenses/agpl-3.0.html
+# test upd
 
 import asyncio
 import io
